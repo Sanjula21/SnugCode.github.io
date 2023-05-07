5e4940996e0603bbe8fe58bc30ced717207be139
Sujaya Nadith <127515360+SnugCode@users.noreply.github.com>
2023-05-07 11:24:04 +0530

    Work cut out for Dinal and Sanjula

---
+++ PHP/quizbackdrop.php
@@ -0,0 +1,16 @@
+<html>
+
+<body>
+    
+<?php 
+    //Write all the php regarding the quiz
+    //Resource our last lab activity 
+    //Make it so that the question appears first and then the answer gets pulled from the form
+    // eg:- Q: Who in invented Ruby?
+    //      A: (Whatever the answer is in the form)
+    //That's all for now. do the same for all ther questions including Student details. 
+?>
+
+</body>
+
+</html>
