672cd22b3518ce8c0ffd0f4ca323b29368ea98aa
Dindi05 <132033832+Dindi05@users.noreply.github.com>
2023-05-08 09:09:24 +0530

    Student details test 1

--- PHP/quizbackdrop.php
+++ PHP/quizbackdrop.php
@@ -1,16 +1,24 @@
 <html>
 
-<body>
-    
-<?php 
-    //Write all the php regarding the quiz
-    //Resource our last lab activity 
-    //Make it so that the question appears first and then the answer gets pulled from the form
-    // eg:- Q: Who in invented Ruby?
-    //      A: (Whatever the answer is in the form)
-    //That's all for now. do the same for all ther questions including Student details. 
-?>
+    <body>
 
-</body>
+    <?php 
+            //Write all the php regarding the quiz
+            //Resource our last lab activity 
+            //Make it so that the question appears first and then the answer gets pulled from the form
+            // eg:- Q: Who in invented Ruby?
+            //      A: (Whatever the answer is in the form)
+    ?>
+
+
+
+
+        Answers of <?php echo $_POST["First Name"]; ?> <?php echo $_POST["Last Name"]; ?><br>
+        Student ID: <?php echo $_POST["Student id"]; ?>
+
+        
+        
+
+    </body>
 
 </html>
